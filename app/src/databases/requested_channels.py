33d33aa4f482b0d89b15fb59ded0a3ecd5bead15
Arash Heidari <malinorev@gmail.com>
2025-08-17 03:01:46 +0330

    Update channel_id type in RequestedChannel model from BigInteger to String for improved data handling.

--- app/src/databases/requested_channels.py
+++ app/src/databases/requested_channels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, Integer
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.database import Base
@@ -13,4 +13,4 @@ class RequestedChannel(Base):
 	}
 
 	id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-	channel_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+	channel_id: Mapped[str] = mapped_column(String(255), nullable=False)
